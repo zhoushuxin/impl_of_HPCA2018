20552c8f5256ce5b69f2b8fb14309a5a72c26d26
zhoushuxin <zhoushuxin@pku.edu.cn>
2019-12-12 16:21:05 +0800

    build/folders

---
+++ build/systemc/.scons_config/conftest_19.c
@@ -0,0 +1,9 @@
+
+int main()
+{
+#ifndef __i386__
+    (void) __i386__;
+#endif
+    ;
+    return 0;
+}
---
+++ build/systemc/.scons_config/conftest_20.c
@@ -0,0 +1,9 @@
+
+int main()
+{
+#ifndef __x86_64__
+    (void) __x86_64__;
+#endif
+    ;
+    return 0;
+}
