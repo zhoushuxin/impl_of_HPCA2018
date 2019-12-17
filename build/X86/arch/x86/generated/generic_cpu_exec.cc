67062de01611b1aac693a093da0dbf87ecc4b767
zhoushuxin <zhoushuxin@pku.edu.cn>
2019-12-17 14:02:35 +0800

    build/X86/arch without large files exceeding 100M

---
+++ build/X86/arch/x86/generated/generic_cpu_exec.cc
@@ -0,0 +1,10 @@
+// DO NOT EDIT
+// This file was automatically generated from an ISA description:
+//   x86/isa/main.isa
+
+#include "exec-g.cc.inc"
+#include "cpu/exec_context.hh"
+#include "decoder.hh"
+namespace X86ISAInst {
+#include "exec-ns.cc.inc"
+}
---
+++ build/X86/arch/x86/generated/inst-constrs.cc
@@ -0,0 +1,9 @@
+// DO NOT EDIT
+// This file was automatically generated from an ISA description:
+//   x86/isa/main.isa
+
+#include "decoder-g.cc.inc"
+#include "decoder.hh"
+namespace X86ISAInst {
+#include "decoder-ns.cc.inc"
+}
